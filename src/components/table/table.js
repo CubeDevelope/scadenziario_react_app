e0672cfa35352fe3883449d813caba567513cd0a
LeoLuca97 <37438630+CubeDevelope@users.noreply.github.com>
2023-08-13 19:34:36 +0200

    First commit

---
+++ src/components/table/table.js
@@ -0,0 +1,13 @@
+import './table.css'
+
+export function Table() {
+  return (
+    <table>
+      <tr>
+        <th>Data</th>
+        <th>Nome procedura</th>
+        <th></th>
+      </tr>
+    </table>
+  );
+}
